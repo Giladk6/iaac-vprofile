39e640ecdebc757e118f8cc09b95e5394c6e07e1
giladk6 <giladk6@gmail.com>
2025-07-15 04:12:03 +0300

    Fix block syntax

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,10 +1,15 @@
 terraform {
   required_providers {
-    aws        = { source = "hashicorp/aws"; version = "~> 6.0" }
-    random     = { source = "hashicorp/random"; version = "~> 3.7" }
-    tls        = { source = "hashicorp/tls"; version = "~> 4.1" }
-    cloudinit  = { source = "hashicorp/cloudinit"; version = "~> 2.3" }
-    kubernetes = { source = "hashicorp/kubernetes"; version = "~> 2.37" }
+    aws        = { source = "hashicorp/aws",
+                   version = "~> 6.0" }
+    random     = { source = "hashicorp/random",
+                   version = "~> 3.7" }
+    tls        = { source = "hashicorp/tls",
+                   version = "~> 4.1" }
+    cloudinit  = { source = "hashicorp/cloudinit",
+                   version = "~> 2.3" }
+    kubernetes = { source = "hashicorp/kubernetes",
+                   version = "~> 2.37" }
   }
 
   required_version = "1.6.3"
