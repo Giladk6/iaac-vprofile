aaae207fe818580257715215486e329ca196a1e8
giladk6 <giladk6@gmail.com>
2025-07-15 05:18:32 +0300

    update aws modules

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,16 +1,36 @@
 terraform {
   required_providers {
-    aws = { source = "hashicorp/aws",
-    version = "~> 6.0" }
-    random = { source = "hashicorp/random",
-    version = "~> 3.7" }
-    tls = { source = "hashicorp/tls",
-    version = "~> 4.1" }
-    cloudinit = { source = "hashicorp/cloudinit",
-    version = "~> 2.3" }
-    kubernetes = { source = "hashicorp/kubernetes",
-    version = "~> 2.37" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.25.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
+    }
   }
 
-  required_version = "1.6.3"
+  backend "s3" {
+    bucket = "iaacprojectvprofile"
+    key    = "terraform.tfstate"
+    region = "il-central-1"
+  }
+
+  required_version = "~> 1.6.3"
 }
